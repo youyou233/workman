f88236f6bec147932370626574bdd4ae6c90b654
youyou <leigned@outlook.com>
2020-12-03 12:02:37 +0800

    add res

--- assets/scripts/utils/config.ts
+++ assets/scripts/utils/config.ts
@@ -8,7 +8,73 @@ let uiName = {
     messageBox: 'messagebox_ui',
 }
 
-let aniConfig = {}
+let aniConfig = {
+    'effect_1': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_2': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_3': {
+        sample: 3, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_4': {
+        sample: 3, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_5': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_6': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_7': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_8': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_9': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_10': {
+        sample: 4, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_11': {
+        sample: 4, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_12': {
+        sample: 4, speed: 2, wrapMode: cc.WrapMode.Normal
+    }, 'effect_13': {
+        sample: 4, speed: 2, wrapMode: cc.WrapMode.Normal
+    }, 'effect_14': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    }, 'effect_15': {
+        sample: 5, speed: 3, wrapMode: cc.WrapMode.Normal
+    }, 'effect_16': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    }, 'effect_17': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    }, 'effect_18': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    }, 'effect_19': {
+        sample: 6, speed: 1, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_21': {
+        sample: 8, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_22': {
+        sample: 5, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_23': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_24': {
+        sample: 6, speed: 2, wrapMode: cc.WrapMode.Normal
+    },
+    'effect_icon': {
+        sample: 8, speed: 2, wrapMode: cc.WrapMode.Loop
+    }
+}
 
 let version = '0.0.1'
 
